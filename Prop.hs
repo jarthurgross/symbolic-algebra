3b72b591dcbd9739cd491d958ff2a1a21420d29d
Jonathan A. Gross <jarthurgross@gmail.com>
2017-05-28 22:47:12 -0600

    Print truth tabls, check tautologies, and clean up

--- Prop.hs
+++ Prop.hs
@@ -1,10 +1,15 @@
 import Data.List
 import qualified Data.Map as Map
+import qualified Data.List.Ordered as Ordered
 
 p = Atom "p"
 q = Atom "q"
+r = Atom "r"
 
-data Table = Table [String]
+data TruthTable = TruthTable { tableAtoms :: [Prop]
+                             , tableProps :: [Prop]
+                             , valuations :: [([Bool], [Bool])]
+                             }
 
 data Prop = T
           | F
@@ -38,24 +43,50 @@ instance Num Prop where
   p1 + p2 = Or p1 p2
   abs p = T
   signum p = p
+  -- fromInteger is included for completeness, but it doesn't make a whole lot
+  -- of sense (i.e. fromInteger (1 * 1) == (fromInteger 1) * (fromInteger 1),
+  -- but fromInteger (1 - 1) /= (fromInteger 1) - (fromInteger 1))
   fromInteger n
     | n `mod` 2 == 0 = F
-    | otherwise    = T
+    | otherwise      = T
   negate p = Not p
 
-instance Show Table where
-  show (Table rows) = intercalate "\n" rows
+infixr 5 ==>
+(==>) :: Prop -> Prop -> Prop
+p ==> q = Imp p q
 
--- Could try refining this to output [Atom]
+infixr 5 <=>
+(<=>) :: Prop -> Prop -> Prop
+p <=> q = Iff p q
+
+-- This could be improved by spacing the valuations of propositions according
+-- to the length of each proposition's representation, but it gets the job done.
+instance Show TruthTable where
+  show tt = intercalate "\n" $ header:divLine:rows
+    where atomHeader = spaceSep $ map show $ tableAtoms tt
+          propHeader = spaceSep $ map show $ tableProps tt
+          header = atomHeader ++ " │ " ++ propHeader
+          divLine = (makeBar atomHeader) ++ "─┼─" ++ (makeBar propHeader)
+          makeBar s = replicate (length s) '─'
+          rowFn val = (spaceSep $ map boolShow $ fst val) ++ " │ " ++
+                      (spaceSep $ map boolShow $ snd val)
+          rows = map rowFn $ valuations tt
+          boolShow True = "⊤" 
+          boolShow False = "⊥"
+          spaceSep = intercalate " "
+
+-- Could try refining this to output [Atom]. Also, this outputs the Atoms sorted
+-- (which is important for dependent functions, such as building a Map from an
+-- AscList with Atoms as keys.).
 atoms :: Prop -> [Prop]
 atoms T = []
 atoms F = []
 atoms (Atom s) = [Atom s]
 atoms (Not p) = atoms p
-atoms (And p q) = union (atoms p) (atoms q)
-atoms (Or p q) = union (atoms p) (atoms q)
-atoms (Imp p q) = union (atoms p) (atoms q)
-atoms (Iff p q) = union (atoms p) (atoms q)
+atoms (And p q) = Ordered.union (atoms p) (atoms q)
+atoms (Or p q) = Ordered.union (atoms p) (atoms q)
+atoms (Imp p q) = Ordered.union (atoms p) (atoms q)
+atoms (Iff p q) = Ordered.union (atoms p) (atoms q)
 
 -- Might also refine the second argument to be from Atom -> Bool
 eval :: Prop -> (Prop -> Bool) -> Bool
@@ -68,27 +99,23 @@ eval (Or p q) v = (eval p v) || (eval q v)
 eval (Imp p q) v = (not $ eval p v) || (eval q v)
 eval (Iff p q) v = (eval p v) == (eval q v)
 
-(==>) :: Prop -> Prop -> Prop
-p ==> q = Imp p q
+truthTable :: [Prop] -> TruthTable
+truthTable ps = TruthTable { tableAtoms = as
+                           , tableProps = ps
+                           , valuations = vals
+                           }
+  where as = foldl union [] $ map atoms ps
+        aVs = allValuations as
+        vals = [(aV, map (flip eval $ valsToFn aV) ps) | aV <- aVs]
+        valsToFn = atomValsToFn . Map.fromAscList . zip as
 
-(<=>) :: Prop -> Prop -> Prop
-p <=> q = Iff p q
-
--- This truth-table code is pretty hackish right now, but it works.
-
-valFromLists :: [Prop] -> [Bool] -> Prop -> Bool
-valFromLists as bs =  flip (Map.findWithDefault False) $ Map.fromAscList kvs
-  where kvs = zip as bs
-
-truthTable :: Prop -> [[Bool]]
-truthTable p = [valList ++ [eval p $ valFromLists as valList]
-               | valList <- valLists]
-  where valLists = sequence $ replicate (length as) [False, True]
+tautology :: Prop -> Bool
+tautology p = and $ (map (flip eval) vs) <*> [p]
+  where vs = atomValsToFn <$> Map.fromAscList <$> (zip as) <$> allValuations as
         as = atoms p
 
-showTruthTable :: Prop -> Table
-showTruthTable p = Table $ [header, (replicate (length header) '―')] ++ values
-  where header = intercalate " " $ (map (\(Atom s) -> s) $ atoms p) ++ [show p]
-        values = map (intercalate " " . (map (\b -> if b then "⊤" else "⊥"))) $
-                 truthTable p
+allValuations :: [Prop] -> [[Bool]]
+allValuations as = sequence $ replicate (length as) [False, True]
 
+atomValsToFn :: Map.Map Prop Bool -> (Prop -> Bool)
+atomValsToFn = flip (Map.findWithDefault False)
