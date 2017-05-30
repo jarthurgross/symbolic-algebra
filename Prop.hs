7da5e89b5826bf3db0dedf8aa567a19e447d7ef1
Jonathan A. Gross <jarthurgross@gmail.com>
2017-05-30 14:36:17 -0600

    Change T and F to Const True and Const False

--- Prop.hs
+++ Prop.hs
@@ -6,13 +6,15 @@ p = Atom "p"
 q = Atom "q"
 r = Atom "r"
 
+t = Const True
+f = Const False
+
 data TruthTable = TruthTable { tableAtoms :: [Prop]
                              , tableProps :: [Prop]
                              , valuations :: [([Bool], [Bool])]
                              }
 
-data Prop = T
-          | F
+data Prop = Const Bool
           | Atom String
           | Not Prop
           | And Prop Prop
@@ -21,8 +23,8 @@ data Prop = T
           | Iff Prop Prop deriving (Eq, Ord)
 
 instance Show Prop where
-  show T = "⊤"
-  show F = "⊥"
+  show (Const True) = "⊤"
+  show (Const False) = "⊥"
   show (Atom s) = s
   show (Not p) = "¬" ++ (show p)
   show (And p1 p2) = "(" ++ (show p1) ++ " ∧ " ++ (show p2) ++ ")"
@@ -31,24 +33,24 @@ instance Show Prop where
   show (Iff p1 p2) = "(" ++ (show p1) ++ " ↔ " ++ (show p2) ++ ")"
 
 instance Num Prop where
-  T * p = p
-  p * T = p
-  F * p = F
-  p * F = p
+  (Const True) * p = p
+  p * (Const True) = p
+  (Const False) * p = Const False
+  p * (Const False) = Const False
   p1 * p2 = And p1 p2
-  T + p = T
-  p + T = T
-  F + p = p
-  p + F = p
+  (Const True) + p = Const True
+  p + (Const True) = Const True
+  (Const False) + p = p
+  p + (Const False) = p
   p1 + p2 = Or p1 p2
-  abs p = T
+  abs p = Const True
   signum p = p
   -- fromInteger is included for completeness, but it doesn't make a whole lot
   -- of sense (i.e. fromInteger (1 * 1) == (fromInteger 1) * (fromInteger 1),
   -- but fromInteger (1 - 1) /= (fromInteger 1) - (fromInteger 1))
   fromInteger n
-    | n `mod` 2 == 0 = F
-    | otherwise      = T
+    | n `mod` 2 == 0 = Const False
+    | otherwise      = Const True
   negate p = Not p
 
 infixr 5 ==>
@@ -58,6 +60,11 @@ p ==> q = Imp p q
 infixr 5 <=>
 (<=>) :: Prop -> Prop -> Prop
 p <=> q = Iff p q
+
+infix 4 |=>
+(|=>) :: Prop -> Prop -> (Prop -> Prop)
+(Atom s) |=> p = subs (Atom s) p
+_ |=> _ = error "Cannot substitute for non-atom"
 
 -- This could be improved by spacing the valuations of propositions according
 -- to the length of each proposition's representation, but it gets the job done.
@@ -80,8 +87,8 @@ instance Show TruthTable where
 -- (which is important for dependent functions, such as building a Map from an
 -- AscList with Atoms as keys.).
 atoms :: Prop -> [Prop]
-atoms T = []
-atoms F = []
+atoms (Const True) = []
+atoms (Const False) = []
 atoms (Atom s) = [Atom s]
 atoms (Not p) = atoms p
 atoms (And p q) = Ordered.union (atoms p) (atoms q)
@@ -91,8 +98,8 @@ atoms (Iff p q) = Ordered.union (atoms p) (atoms q)
 
 -- Might also refine the second argument to be from Atom -> Bool
 eval :: Prop -> (Prop -> Bool) -> Bool
-eval T _ = True
-eval F _ = False
+eval (Const True) _ = True
+eval (Const False) _ = False
 eval (Atom s) v = v (Atom s)
 eval (Not p) v = not $ eval p v
 eval (And p q) v = (eval p v) && (eval q v)
@@ -129,8 +136,8 @@ atomValsToFn = flip (Map.findWithDefault False)
 
 -- Should refine the first argument to be an Atom
 subs :: Prop -> Prop -> Prop -> Prop
-subs _ _ T = T
-subs _ _ F = F
+subs _ _ (Const True) = Const True
+subs _ _ (Const False) = Const False
 subs (Atom s) asub (Atom s') = if s == s' then asub else (Atom s')
 subs a asub (Not p) = Not $ subs a asub p
 subs a asub (And p q) = And (subs a asub p) (subs a asub q)
