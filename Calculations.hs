4c2dedd85b82afa47b86ecc6f61e06d96fce217d
Jonathan A. Gross <jarthurgross@gmail.com>
2017-06-24 13:52:18 -0600

    Add code to check squeezing calculations

--- Calculations.hs
+++ Calculations.hs
@@ -73,3 +73,79 @@ boutdagbout = collectOnB $ truncateToOrderOpAB sqrtdt 3 $ (dag bout) /*/ bout
 
 ket0 = VecVar "0"
 ket1 = VecVar "1"
+
+phase = Var "ω"
+
+supOpD :: Algebra a => a -> a -> a
+supOpD c x = c /*/ x /*/ (dag c) /+/ (negate half) */
+             ((dag c) /*/ c /*/ x /+/ x /*/ (dag c) /*/ c)
+
+n = RealVar "N"
+nth = Const 0
+-- nth = RealVar "Nₜₕ"
+m = Var "M"
+
+l' = 2 * n + m + (conjScalar m) + 1
+
+csq = (conjScalar phase) */ ((nth + n + (conjScalar m) + 1) */ c /+/
+      (nth - n - m) */ (dag c))
+
+cr = RealVar "coshr"
+sr = RealVar "sinhr"
+
+csq' = cr */ c /+/ (phase * sr) */ cdg
+
+supOpDcsq' = (cr^2) */ (supOpD c rho) /+/ (sr^2) */ (supOpD cdg rho) /+/
+             (-half * (conjScalar phase) * cr * sr) */ (comm c $ comm c rho) /+/
+             (-half * phase * cr * sr) */ (comm cdg $ comm cdg rho)
+
+comm :: Algebra a => a -> a -> a
+comm a b = a /*/ b /+/ (-1) */ b /*/ a
+
+supOpDcsqCandidate = l' */ ((n + nth + 1) */ (supOpD c rho) /+/
+                     (n + (-1) * nth) */ (supOpD (dag c) rho) /+/
+                     (half * (conjScalar m)) */ (comm c $ comm c rho) /+/
+                     (half * m) */ (comm (dag c) $ comm (dag c) rho))
+
+simplifyPhaseScalar :: Scalar -> Scalar
+simplifyPhaseScalar (Neg sca) = negate $ simplifyPhaseScalar sca
+simplifyPhaseScalar (Conj sca) = conjScalar $ simplifyPhaseScalar sca
+simplifyPhaseScalar (Pow sca n) = Pow (simplifyPhaseScalar sca) n
+simplifyPhaseScalar (Abs sca) = abs $ simplifyPhaseScalar sca
+simplifyPhaseScalar (Sgn sca) = signum $ simplifyPhaseScalar sca
+simplifyPhaseScalar (Tr op) = Tr $ simplifyPhaseOp op
+simplifyPhaseScalar (TrAB op) = TrAB $ simplifyPhaseOpAB op
+simplifyPhaseScalar (Add scas) = sum $ map simplifyPhaseScalar scas
+simplifyPhaseScalar (Mul scas) = product $ cancelPhases $
+                                 map simplifyPhaseScalar scas
+simplifyPhaseScalar sca = sca
+
+cancelPhases :: [Scalar] -> [Scalar]
+cancelPhases scas = nonPhases ++ if balance > 0 then replicate balance phase
+                    else replicate (-balance) $ conjScalar phase
+  where balance = (length phases) - (length conjPhases)
+        phases = [sca | sca <- scas, sca == phase]
+        conjPhases = [sca | sca <- scas, sca == conjScalar phase]
+        nonPhases = [sca | sca <- scas, sca /= phase && sca /= conjScalar phase]
+
+simplifyPhaseOp :: Op -> Op
+simplifyPhaseOp (Dag op) = dag $ simplifyPhaseOp op
+simplifyPhaseOp (SMul sca op) = (simplifyPhaseScalar sca) */
+                                (simplifyPhaseOp op)
+simplifyPhaseOp (PowOp op n) = PowOp (simplifyPhaseOp op) n
+simplifyPhaseOp (TrA op) = TrA $ simplifyPhaseOpAB op
+simplifyPhaseOp (TrB op) = TrB $ simplifyPhaseOpAB op
+simplifyPhaseOp (AddOp ops) = algSum $ map simplifyPhaseOp ops
+simplifyPhaseOp (MulOp ops) = algProd $ map simplifyPhaseOp ops
+simplifyPhaseOp op = op
+
+simplifyPhaseOpAB :: OpAB -> OpAB
+simplifyPhaseOpAB (DagAB op) = dag $ simplifyPhaseOpAB op
+simplifyPhaseOpAB (SMulAB sca op) = (simplifyPhaseScalar sca) */
+                                    (simplifyPhaseOpAB op)
+simplifyPhaseOpAB (PowOpAB op n) = PowOpAB (simplifyPhaseOpAB op) n
+simplifyPhaseOpAB (AddOpAB ops) = algSum $ map simplifyPhaseOpAB ops
+simplifyPhaseOpAB (MulOpAB ops) = algProd $ map simplifyPhaseOpAB ops
+simplifyPhaseOpAB (TProd opa opb) = TProd (simplifyPhaseOp opa)
+                                    (simplifyPhaseOp opb)
+simplifyPhaseOpAB op = op
