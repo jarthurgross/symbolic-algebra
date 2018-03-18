94364f101ba8027ca8121124e88ba2f1a337ba01
Jonathan A. Gross <jarthurgross@gmail.com>
2018-03-18 15:19:58 -0700

    Add fn to simplify phase products

--- SqueezedBasisElim.hs
+++ SqueezedBasisElim.hs
@@ -26,7 +26,25 @@ yt = simplifyNt $ ((Const (-2)) * (sqrtgam^^(-2)) ) */ p1 /*/ (nt /-/
      (sqrtgam^^(-2)) */ nt /*/ (esq */ adg /*/ adg /-/
      (conjScalar esq) */ a /*/ a) /*/ nt) /*/ p1
 
--- simplifyPhaseMag :: Scalar -> Scalar
--- TODO: Write a function that simplifies products of the phase with it's
--- conjugate to
--- 1.
+-- Simplify products of a phase and its conjugate to unity.
+collectPhases :: Scalar -> Scalar -> Scalar
+collectPhases ph (Mul scas) = (if phase_count > 0 then ph ^ phase_count
+                               else if phase_count < 0
+                               then (conjScalar ph) ^ (-phase_count)
+                               else Const 1) * (product xs)
+  where (xs, phase_count) = countPhases ph scas
+collectPhases ph sca = travScaSca (collectPhases ph) sca
+
+countPhases :: Scalar -> [Scalar] -> ([Scalar], Integer)
+countPhases ph scas = foldr sortPhases ([], 0) scas
+  where sortPhases sca@(Pow base n) (scas, phase_count)
+          | base == ph            = (scas, phase_count + n)
+          | base == conjScalar ph = (scas, phase_count - n)
+          | otherwise             = (sca:scas, phase_count)
+        sortPhases sca (scas, phase_count)
+          | sca == ph            = (scas, phase_count + 1)
+          | sca == conjScalar ph = (scas, phase_count - 1)
+          | otherwise            = (sca:scas, phase_count)
+
+collectPhasesOp :: Scalar -> Op -> Op
+collectPhasesOp ph = travScaOp (collectPhases ph)
